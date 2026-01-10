9777a2c04b56c4abe77cbae2660e434b90c1a480
Frank Van Geirt <frank.van.geirt@telenet.be>
2026-01-10 08:53:21 +0100

    Move checkboxes to settings screen.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import kivy
+from kivy.properties import AliasProperty, ObjectProperty
 
 kivy.require('2.3.1')
 
@@ -9,15 +10,14 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 
 from prompt import Prompt
 
-Builder.load_file('main.kv')
-
-
 class HomeScreen(Screen):
     def on_button_create(self):
+        weighted_value = self.manager.get_screen('settings').ids.chk_weighted.active
+        randomized_value = self.manager.get_screen('settings').ids.chk_randomized.active
         prompt = Prompt(
             text=self.ids.prompt_start.text,
-            weighted=self.ids.checkbox_weighted.active,
-            randomize=self.ids.checkbox_randomized.active)
+            weighted=weighted_value,
+            randomize=randomized_value)
         self.ids.prompt_new.text = prompt.create_prompt()
 
     def on_button_copy(self):
@@ -25,17 +25,18 @@ class HomeScreen(Screen):
 
 
 class SettingsScreen(Screen):
+    chk_weighted = ObjectProperty(None)
+    chk_randomized = ObjectProperty(None)
+
+class WindowManager(ScreenManager):
     pass
 
+kv = Builder.load_file('main.kv')
 
 class MyApp(App):
     def build(self):
         self.title = "SD Prompt Engineering"
-        sm = ScreenManager()
-        sm.add_widget(HomeScreen(name='home'))
-        sm.add_widget(SettingsScreen(name='settings'))
-        return sm
-
+        return kv
 
 if __name__ == '__main__':
     MyApp().run()
